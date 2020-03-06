b344dfb15d819c64b7bc229431d94544ebce8071
cclzhang <hello@cecilezhang.com>
2020-03-06 10:56:23 -0500

    created maze component

---
+++ src/components/Maze.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Maze extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div className="maze">
+                <h3>maze is here</h3>
+                <button onClick="">take me to next page</button>
+            </div>
+        )
+    }
+}
+
+export default Maze;
