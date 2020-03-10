8ea433b0cfb7e1266de84014a84f858baf09f910
cclzhang <hello@cecilezhang.com>
2020-03-10 11:49:47 -0400

    added basic leaderboard and firebase functionality

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,26 +1,56 @@
-import React, { Fragment } from 'react';
-import '../styles/ResultsPage.css'
+import React, {Component} from 'react';
+import firebase from '../data/firebase';
+import Leaderboard from './Leaderboard';
+import '../styles/ResultsPage.css';
+
 
 // stateless component
-const ResultsPage = (props) => {
-  return (
-    <Fragment>
-      <div className="wrapper">
-        <div className="nygmaContainer">
-          {/* image here :) - image of Nygma Machine
-            will need to figure out what the Nygma Machine looks  */}
+class ResultsPage extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userCompleted: [],
+    }
+  }
+  
+  componentDidMount(){
+    const dbRef = firebase.database().ref();
+    dbRef.on('value', (response) => {
+      const info = response.val();
+      const userName = [];
+      for (let key in info) {
+        userName.push(info[key].name);
+      }
+      this.setState({
+        userCompleted: userName,
+      })
+    })
+  }
+  render(){
+    return (
+        <div className="wrapper">
+          <div className="nygmaContainer">
+            {/* image here :) - image of Nygma Machine
+              will need to figure out what the Nygma Machine looks  */}
+              
+            <div className="quoteContainer">
+              <p className="resultsName">{`${this.props.userName},`}</p>
+              <p className="resultsQuote">{this.props.quote}</p> 
+              <ul>
+                {this.state.userCompleted.map((name, index)=>{
+                  return(
+                    <li key={index}>{name}</li>
+                  )
+                })}
+              </ul>
+            </div>
             
-          <div className="quoteContainer">
-            <p className="resultsName">{`${props.userName},`}</p>
-            <p className="resultsQuote">{props.quote}</p> 
+            <button onClick={this.props.updatePage}>Play Again?</button>
+            <button>Leaderboard</button>
           </div>
-          
-          <button onClick={props.updatePage}>Play Again?</button>
-          <button>Leaderboard</button>
         </div>
-      </div>
-    </Fragment>
-  )
+    )
+  }
 }
 
 export default ResultsPage;
