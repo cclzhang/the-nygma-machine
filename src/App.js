a4b76b50323cd89d5030a57f19fbbce2256b7d36
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-03-11 11:27:16 -0400

    wrapped up documentation, organized function flow for app.js file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,46 @@ import React, {Component} from 'react';
 import './styles/App.css';
 import axios from 'axios';
 import firebase from './data/firebase';
+
 import Preloader from './components/Preloader';
 import LandingPage from './components/LandingPage';
 import Maze from './components/Maze'
 import ResultsPage from './components/ResultsPage';
 
 
+// user experience
+// userInput for name and question is sent from landing component to here (app) and stored
+// upon submission event in landing component, api call is made and stored, and maze component renders for user
+// user can interact with maze using 'wasd' keys, arrow keys, or by touch (on mobile)
+// upon completion of maze, stored name is sent to firebase, and results component renders for user
+// results page will display quote from api call, as well as the option for user to replay the game, or see the leaderboard
+
+// component breakdown (in order of operation)
+// App.js
+// Preloader.js
+// LandingPage.js
+// Maze.js
+// ResultsPage.js
+// Leaderboard.js
+
+// App.js breakdown
+// constructor
+// storeUserName (f)
+// storeUserQuestion (f)
+  // value lookup from string logic
+  // api call and resulting actions
+// firebase store (f)
+// replay (f)
+// preloader (f)
+// render
+
+
 class App extends Component {
   constructor(){
     super();
 
+    // due to flow of app, rather than use router we displayed components using boolen values
+    // quote and name keys are used to store values from submit event handler in landing page component, which is sent here (app), followed by results component
     this.state = {
       isPreloaderShown: true,
       isLandingShown: true,
@@ -22,27 +52,7 @@ class App extends Component {
     }
   }
 
-  updateResult = () => {
-    const dbRef = firebase.database().ref();
-    const userDeets = {
-      name: this.state.name,
-      quote: this.state.quote,
-    }
-    dbRef.push(userDeets); 
-
-    this.setState({
-      isMazeShown: false,
-      isResultsShown: true,
-    });
-  }
-
-  replay = () => {
-    this.setState({
-      isResultsShown: !this.state.isResultsShown,
-      isLandingShown: !this.state.isLandingShown,
-    });
-  }
-
+  // userInput (landing component via props) will store in state, which is sent to results component
   storeUserName = (e, userNameInput) => {
     e.preventDefault();
     this.setState({
@@ -50,6 +60,7 @@ class App extends Component {
     })
   }
 
+  // userInput (landing component via props) will be used in API call and will store in state, which is sent to results component
   storeUserQuestion = (e, question) => {
     e.preventDefault();
 
@@ -60,18 +71,18 @@ class App extends Component {
     // if the randomly generated word doesn't exist in the badWords array, it'll be used as the lookup value in the axios call
     const getRandomWord = (question) => {
       const randomIndex = Math.floor(Math.random() * question.length);
+      const keyWord = question[randomIndex];
+
       const badWords = [
         // common
-        "the", "to", "and", "then", "this", "of", "is", "if", "it", "so", "a", "is", "maybe", "on", "for",
+        'the', 'to', 'and', 'then', 'this', 'of', 'is', 'if', 'it', 'so', 'a', 'is', 'maybe', 'on', 'for',
         // pronouns
-        "i", "i'm", "me", "you", "them", "they", "we", "it",
-        // grammar (revist this)
-        "!", "'", "\"",
+        'i', 'i\'m', 'me', 'you', 'them', 'they', 'we',
+        // grammar
+        '!', '\'', '"',
       ]
 
-      const keyWord = question[randomIndex];
       const keyWordLookup = badWords.includes(keyWord);
-
       if (keyWordLookup === false) {
         return keyWord;
       }
@@ -79,7 +90,7 @@ class App extends Component {
       getRandomWord(question);
     }
 
-    // storing the lookup value into a variable, used in the axios call
+    // storing the lookup value into a variable, which is used in the axios call
     const returnedKeyword = getRandomWord(question);
 
     axios({
@@ -89,6 +100,7 @@ class App extends Component {
       // if the initial axios call doesn't have a quote for the lookup value, it'll generate a random advice string
       // otherwise, the axios call will return a random advice string related to the lookup value
       // in both instances, the random advice string is stored in state, to be returned to the App.js component
+      // regardless of condition, maze component is rendered
       if (response.data.message) {
         axios({
           url: 'https://api.adviceslip.com/advice',
@@ -111,6 +123,28 @@ class App extends Component {
     });
   }
 
+  updateResult = () => {
+    const dbRef = firebase.database().ref();
+    const userDeets = {
+      name: this.state.name,
+      quote: this.state.quote,
+    }
+    dbRef.push(userDeets);
+
+    this.setState({
+      isMazeShown: false,
+      isResultsShown: true,
+    });
+  }
+
+  // replay function will reset which components are displayed
+  replay = () => {
+    this.setState({
+      isResultsShown: !this.state.isResultsShown,
+      isLandingShown: !this.state.isLandingShown,
+    });
+  }
+
   componentDidMount(){
     setTimeout(()=> { 
       this.setState({
@@ -119,6 +153,11 @@ class App extends Component {
     }, 2300);
   }
   
+  // by default, we load landing component
+  // all other components naturally render false, which is changed upon user input
+  // landing component will send intial user input here (app) via props
+  // after storing in state here (app), it is sent to results page via props
+  // completion of maze will send 
   render(){
     return (
       <div className="App">
@@ -133,3 +172,4 @@ class App extends Component {
 }
 
 export default App;
+
